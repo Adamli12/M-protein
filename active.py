393b4c14e12f481a2813ac4bb8dc77d75b663bb7
Adamli12 <l-hy12@outlook.com>
2019-08-03 16:32:38 -0400

    preparing for SV

---
+++ active.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from sklearn.mixture import BayesianGaussianMixture
+from sklearn.mixture import GaussianMixture
+import os,sys
+from sklearn.svm import SVC
+
+#need to implement an SVM with incremental property and yield margin distance
+
+
--- main.py
+++ main.py
@@ -305,20 +305,16 @@ def read_label(path,ans):
 def classify_folder(path,pre,gt=None,testflag=0,cut_n=6,numsort=0):
     i=0
     if numsort==0:
-        train=[]
+        #train=[]
         test=[]
     else:
-        train=np.zeros((len(os.listdir(path)),45))
+        #train=np.zeros((len(os.listdir(path)),45))
         test=np.zeros((len(os.listdir(path)),12))
     for img in os.listdir(path):
         if numsort==1:
             nu=int(img[:-4])
         path1=os.path.join(path,img)
         ans=BGMreport(path1,0,cut_n)
-        if numsort==0:
-            train.append(ans[1])
-        else:
-            train[nu]=(ans[1])
         if testflag==1:
             if numsort==0:
                 test.append(ans[0]==gt[i])
@@ -332,8 +328,25 @@ def classify_folder(path,pre,gt=None,testflag=0,cut_n=6,numsort=0):
         i+=1
         if i%20==0:
             print(i)
-    np.savetxt(pre+"components.csv",train,delimiter="\t",fmt="%.4f")
+    #np.savetxt(pre+"components.csv",train,delimiter="\t",fmt="%.4f")
     np.savetxt(pre+"labels.csv",test,delimiter="\t",fmt="%d")
+    return 0
+
+def folder_to_data(path,pre,cut_n=6):
+    i=0
+    train=np.zeros((len(os.listdir(path)*5),9))
+    test=np.zeros((len(os.listdir(path)*5)))
+    for img in os.listdir(path):
+        path1=os.path.join(path,img)
+        ans=BGMreport(path1,0,cut_n)
+        for j in range(5):
+            train[i*5+j]=ans[1][j*9:j*9+9]
+            test[i*5+j]=ans[0][7+j]
+        i+=1
+        if i%20==0:
+            print(i)
+    np.savetxt(pre+"train.csv",train,delimiter="\t",fmt="%.4f")
+    np.savetxt(pre+"test.csv",test,delimiter="\t",fmt="%d")
     return 0
 
 def generate_pics(pathgk,pathno,num):
@@ -379,7 +392,9 @@ classify_folder("generate_gkpics","gk",cut_n=5,numsort=1)
 classify_folder("generate_nopics","no",cut_n=5,numsort=1)"""
 print(read_label("nolabels.csv",[0,0,0,0,0]))
 
-ans=BGMreport("pics/trainpics/j1.jpg",1,cut_n=6)
+folder_to_data("pics/trainpics","t",6)
+
+ans=BGMreport("pics/trainpics/b.jpg",1,cut_n=6)
 print(ans[0])
 #print(ans[1])
 
