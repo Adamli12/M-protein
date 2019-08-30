cd5b2bc597c30b4e5e85e614ad3a703eb4bfde99
Adamli12 <l-hy12@outlook.com>
2019-08-30 00:39:30 -0400

    vae run through

--- vae.py
+++ vae.py
@@ -7,18 +7,20 @@ from torch import nn, optim
 from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
-
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
+parser.add_argument('--batch-size', type=int, default=4, metavar='N',
+                    help='input batch size for training (default: 4)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=5, metavar='N',
                     help='how many batches to wait before logging training status')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -52,10 +54,8 @@ class Mvaedataset(data.Dataset):
 
 
 class VAE(nn.Module):
-    def __init__(self,dat):
+    def __init__(self):
         super(VAE, self).__init__()
-        self.dataset=Mvaedataset(dat)
-        self.dataloader=data.DataLoader(dataset=self.dataset, batch_size=self.batch_size, shuffle=True)
         self.fc1 = nn.Linear(300, 150)
         self.fc21 = nn.Linear(150, 9)
         self.fc22 = nn.Linear(150, 9)
@@ -80,45 +80,58 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
-
-model = VAE().to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-
 # Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 300), reduction='sum')
 
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+class VAEmodel():
+    def __init__(self, dat,args):
+        self.dataset = Mvaedataset(dat)
+        self.batch_size = args.batch_size
+        self.dataloader = data.DataLoader(dataset=self.dataset, batch_size = self.batch_size, shuffle = True)
+        self.epoch_num = args.epochs
+        self.module=VAE().to(device)
+        self.optimizer = optim.Adam(self.module.parameters(), lr=1e-3)
 
-    return BCE + KLD
+    def loss_function(self,recon_x, x, mu, logvar):
+        BCE = F.binary_cross_entropy(recon_x, x.view(-1, 300), reduction='sum')
 
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-def train(epoch):
-    model.train()
-    train_loss = 0
-    for batch_idx, data in enumerate(self.dataloader):
-        data = data.to(device)
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(dataloader.dataset),
-                100. * batch_idx / len(dataloader),
-                loss.item() / len(data)))
+        return BCE + KLD
 
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(dataloader.dataset)))
+    def train(self):
+        self.module.train()
+        for epoch in range(1,self.epoch_num+1):
+            train_loss = 0
+            for batch_idx, data in enumerate(self.dataloader):
+                data = data.to(device)
+                data=torch.tensor(data, dtype=torch.float32)
+                self.optimizer.zero_grad()
+                recon_batch, mu, logvar = self.module(data)
+                loss = self.loss_function(recon_batch, data, mu, logvar)
+                loss.backward()
+                train_loss += loss.item()
+                self.optimizer.step()
+                if batch_idx % args.log_interval == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data), len(self.dataloader.dataset),
+                        100. * batch_idx / len(self.dataloader),
+                        loss.item() / len(data)))
 
-def svmtest():
+            print('====> Epoch: {} Average loss: {:.4f}'.format(
+                epoch, train_loss / len(self.dataloader.dataset)))
+
+    def svmtest(self,testdata,testlabel,svm):
+        self.module.eval()
+        with torch.no_grad():
+            td=torch.tensor(testdata,dtype=torch.float32).to(device)
+            recon_batch, mu, logvar = self.module(td)
+        svmdat=mu.view(len(testdata),9).detach().numpy()
+        svm.partial_fit(svmdat,testlabel,classes=[0,1])
+        print("the latest train dataset score",svm.score(svmdat,testlabel))
     
 
 """def test(epoch):
@@ -140,9 +153,31 @@ def svmtest():
     print('====> Test set loss: {:.4f}'.format(test_loss))"""
 
 if __name__ == "__main__":
-    for epoch in range(1, args.epochs + 1):
-        train(epoch)
-        #test(epoch)
-        """with torch.no_grad():
-            sample = torch.randn(64, 9).to(device)
-            sample = model.decode(sample).cpu()"""
+    unlabeledpath="vaetrain.csv"
+    featurepath="vaetrain.csv"
+    labelpath="vaelabel.csv"
+    testfeaturepath="vaetrain.csv"
+    testlabelpath="vaelabel.csv"
+    feature=np.loadtxt(featurepath,delimiter="\t")
+    label=np.loadtxt(labelpath,delimiter="\t")
+    ufeature=np.loadtxt(unlabeledpath,delimiter="\t")
+    testfeature=np.loadtxt(testfeaturepath,delimiter="\t")
+    testlabel=np.loadtxt(testlabelpath,delimiter="\t")
+
+    scaler=StandardScaler()
+    ufeature_sc=scaler.fit_transform(ufeature)#using large unlabeled data to normalize
+    feature_sc=scaler.transform(feature)
+    testfeature_sc=scaler.transform(testfeature)
+
+    svm=SGDClassifier(max_iter=2500)
+
+    vamo=VAEmodel(ufeature_sc,args)
+    vamo.train()
+    vamo.svmtest(testfeature_sc,testlabel,svm)
+        
+    #test(epoch)
+    """
+    with torch.no_grad():
+        sample = torch.randn(64, 9).to(device)
+        sample = model.decode(sample).cpu()
+    """
